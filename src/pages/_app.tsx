0ccf34da6307506f8833d1f89ccbaa496620bcc5
taylanturker-v <taylanturker@sabanciuniv.edu>
2023-12-06 21:16:29 +0300

    audio player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,9 +26,11 @@ export default function App({ Component, pageProps }: AppProps) {
 			
 
 			
-
-			
-			
+    <AudioPlayer
+						filename="DemoSongs/Song1.mp3"
+						trackName="Tower Block Dreams Show"
+						imgSrc="DemoSongs/tou.jpeg"
+					/>
 		</div>
       <Component {...pageProps} />
     </>
